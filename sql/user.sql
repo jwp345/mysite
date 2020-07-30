eeb28cc07f08a22690787ad53363351d51200331
jwp345 <jwp135@naver.com>
2020-07-30 17:39:28 +0900

    sql

---
+++ sql/user.sql
@@ -0,0 +1,11 @@
+desc user;
+-- select
+select * from user;
+
+-- 회원 가입
+insert into user values(null, '편재원', 'jwp135@naver.com', password('1234'), 'male', now());
+
+-- login
+select no, name from user where email='jwp135@naver.com' and password=password('1234');
+
+delete from user where no is null;
